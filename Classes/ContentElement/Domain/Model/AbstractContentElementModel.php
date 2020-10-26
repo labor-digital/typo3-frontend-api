5cc51905eeba156206b455423ab6724b99654535
Martin Neundorfer <m.neundorfer@labor.digital>
2020-10-26 20:16:18 +0100

    feat(AbstractContentElementModel): add $remapVCols to getRaw()

--- Classes/ContentElement/Domain/Model/AbstractContentElementModel.php
+++ Classes/ContentElement/Domain/Model/AbstractContentElementModel.php
@@ -24,66 +24,110 @@ use Neunerlei\Arrays\Arrays;
 use Neunerlei\Inflection\Inflector;
 use TYPO3\CMS\Extbase\DomainObject\AbstractEntity;
 
-abstract class AbstractContentElementModel extends AbstractEntity {
-	
-	/**
-	 * The map of column names and their virtual column name to map
-	 * @var array
-	 */
-	protected $__virtualColumnMap;
-	
-	/**
-	 * The raw database array that was used to create this model
-	 * @var array
-	 */
-	protected $__raw;
-	
-	/**
-	 * Contains
-	 * @var array
-	 */
-	protected $__flex = [];
-	
-	/**
-	 * Returns the raw database array that was used to create this model
-	 * @return array
-	 */
-	public function getRaw(): array {
-		return $this->__raw;
-	}
-	
-	/**
-	 * Can be used to access the values of any flex form field in your configuration.
-	 * By default this method returns the full flex form array (if no path is given).
-	 * The method will combine all available flex form fields into a single array, that contains the matching field
-	 * names. You can use the $path attribute to select a specific path in your configuration
-	 *
-	 * @param array|string|null $path    If empty the whole flex form array is returned. Can be used to find a
-	 *                                   sub-section of the configuration.
-	 * @param null|mixed        $default Can be used to define a default value that is returned inf the given path was
-	 *                                   not found.
-	 *
-	 * @return array|mixed|null
-	 */
-	public function getFlex($path = NULL, $default = NULL) {
-		if (empty($path)) return $this->__flex;
-		return Arrays::getPath($this->__flex, $path, $default);
-	}
-	
-	/**
-	 * Allow magic access to all the raw properties of this model
-	 *
-	 * @param $name
-	 *
-	 * @return mixed|null
-	 */
-	public function __get($name) {
-		$raw = $this->getRaw();
-		if (isset($this->__virtualColumnMap[$name])) $name = $this->__virtualColumnMap[$name];
-		if (isset($raw[$name])) return $raw[$name];
-		$name = Inflector::toDatabase($name);
-		if (isset($this->__virtualColumnMap[$name])) $name = $this->__virtualColumnMap[$name];
-		if (isset($raw[$name])) return $raw[$name];
-		return NULL;
-	}
+abstract class AbstractContentElementModel extends AbstractEntity
+{
+
+    /**
+     * The map of column names and their virtual column name to map
+     *
+     * @var array
+     */
+    protected $__virtualColumnMap;
+
+    /**
+     * The raw database array that was used to create this model
+     *
+     * @var array
+     */
+    protected $__raw;
+
+    /**
+     * The raw database array where the vCols have been remapped to their shorter name
+     *
+     * @var array
+     */
+    protected $__rawRemapped;
+
+    /**
+     * Contains
+     *
+     * @var array
+     */
+    protected $__flex = [];
+
+    /**
+     * Returns the raw database array that was used to create this model
+     *
+     * @return array
+     */
+    public function getRaw(bool $remapVCols = false): array
+    {
+        if ($remapVCols) {
+            if (is_array($this->__rawRemapped)) {
+                return $this->__rawRemapped;
+            }
+
+            $this->__rawRemapped = $this->__raw;
+            $map                 = array_flip($this->__virtualColumnMap);
+            foreach ($this->__rawRemapped as $field => $value) {
+                if (isset($map[$field])) {
+                    $field = $map[$field];
+                }
+                $this->__rawRemapped[$field] = $value;
+            }
+
+            return $this->__rawRemapped;
+        }
+
+        return $this->__raw;
+    }
+
+    /**
+     * Can be used to access the values of any flex form field in your configuration.
+     * By default this method returns the full flex form array (if no path is given).
+     * The method will combine all available flex form fields into a single array, that contains the matching field
+     * names. You can use the $path attribute to select a specific path in your configuration
+     *
+     * @param   array|string|null  $path     If empty the whole flex form array is returned. Can be used to find a
+     *                                       sub-section of the configuration.
+     * @param   null|mixed         $default  Can be used to define a default value that is returned inf the given path was
+     *                                       not found.
+     *
+     * @return array|mixed|null
+     */
+    public function getFlex($path = null, $default = null)
+    {
+        if (empty($path)) {
+            return $this->__flex;
+        }
+
+        return Arrays::getPath($this->__flex, $path, $default);
+    }
+
+    /**
+     * Allow magic access to all the raw properties of this model
+     *
+     * @param $name
+     *
+     * @return mixed|null
+     */
+    public function __get($name)
+    {
+        $raw = $this->getRaw();
+        if (isset($this->__virtualColumnMap[$name])) {
+            $name = $this->__virtualColumnMap[$name];
+        }
+        if (isset($raw[$name])) {
+            return $raw[$name];
+        }
+        $name = Inflector::toDatabase($name);
+        if (isset($this->__virtualColumnMap[$name])) {
+            $name = $this->__virtualColumnMap[$name];
+        }
+        if (isset($raw[$name])) {
+            return $raw[$name];
+        }
+
+        return null;
+    }
 }
